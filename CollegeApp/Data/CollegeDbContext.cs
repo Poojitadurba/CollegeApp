d33e94c5ad3c2445e5f7f916386f30f9370cb3d0
Poojitadurba <poojitha.durba@gmail.com>
2025-06-07 13:44:17 +0530

    Writing Entity Type Configurations in seperate file

--- CollegeApp/Data/CollegeDbContext.cs
+++ CollegeApp/Data/CollegeDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using CollegeApp.Data.Config;
+using Microsoft.EntityFrameworkCore;
 
 namespace CollegeApp.Data
 {
@@ -12,11 +13,20 @@ namespace CollegeApp.Data
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<Student>().HasData(new List<Student>()
-            {
-                new Student{Id=1,StudentName="Poojita",Address="Hyderabad",Email="abc@gmail.com"},
-                new Student{Id=2,StudentName="Keerthi Kalyan",Address="Coimbatore",Email="xyz@gmail.com"}
-            });
+            //modelBuilder.Entity<Student>().HasData(new List<Student>()
+            //{
+            //    new Student{Id=1,StudentName="Poojita",Address="Hyderabad",Email="abc@gmail.com"},
+            //    new Student{Id=2,StudentName="Keerthi Kalyan",Address="Coimbatore",Email="xyz@gmail.com"}
+            //});
+
+            //modelBuilder.Entity<Student>(entity =>
+            //{
+            //    entity.Property(n => n.StudentName).IsRequired();
+            //    entity.Property(n => n.StudentName).HasMaxLength(250);
+            //    entity.Property(n => n.Address).IsRequired(false).HasMaxLength(500);
+            //    entity.Property(n => n.Email).IsRequired().HasMaxLength(250);
+            //});
+            modelBuilder.ApplyConfiguration(new StudentConfig());
         }
     }
 }
---
+++ CollegeApp/Data/Config/StudentConfig.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CollegeApp.Data.Config
+{
+    public class StudentConfig : IEntityTypeConfiguration<Student>
+    {
+        public void Configure(EntityTypeBuilder<Student> builder)
+        {
+            builder.ToTable("students");
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Id).UseIdentityColumn();
+            builder.Property(n => n.StudentName).IsRequired();
+            builder.Property(n => n.StudentName).HasMaxLength(250);
+            builder.Property(n => n.Address).IsRequired(false).HasMaxLength(500);
+            builder.Property(n => n.Email).IsRequired().HasMaxLength(250);
+            builder.HasData(new List<Student>()
+            {
+                new Student{Id=1,StudentName="Poojita",Address="Hyderabad",Email="abc@gmail.com"},
+                new Student{Id=2,StudentName="Keerthi Kalyan",Address="Coimbatore",Email="xyz@gmail.com"}
+            });
+        }
+    }
+}
