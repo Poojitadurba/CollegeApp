ad0ae41681aa402b15c6a70a0d56a46dca8ba6a2
Poojitadurba <poojitha.durba@gmail.com>
2025-06-08 16:22:41 +0530

    Auto Mapper Options

--- CollegeApp/Configurations/AutoMapperConfig.cs
+++ CollegeApp/Configurations/AutoMapperConfig.cs
@@ -10,7 +10,18 @@ namespace CollegeApp.Configurations
         {
             //CreateMap<Student,StudentDTO>();
             //CreateMap<StudentDTO, Student>();
-            CreateMap<Student, StudentDTO>().ReverseMap();
+
+            CreateMap<StudentDTO, Student>().ReverseMap();
+
+            //Config if you have different property names
+            //CreateMap<Student, StudentDTO>().ReverseMap().ForMember(n => n.Name, opt => opt.MapFrom(x => x.StudentName));
+
+            //Config if you want to ignore mapping any property
+            //CreateMap<StudentDTO, Student>().ReverseMap().ForMember(n => n.StudentName, opt => opt.Ignore());
+
+            //Config if you want to print some message instead of null in any field.
+           CreateMap<StudentDTO, Student>().ForMember(n=>n.Address,opt=>opt.
+           MapFrom(n=>string.IsNullOrEmpty(n.Address)?"No address found":n.Address)).ReverseMap().AddTransform<string>(n => string.IsNullOrEmpty(n) ? "No address found" : n);
         }
     }
 }
